1ee872368bafcc191ed2e000d3023ee4c36ebdd4
Igor <Alyokhini@mail.ru>
2021-08-12 20:20:26 +0300

    add cycle

--- script.js
+++ script.js
@@ -3,9 +3,6 @@
 const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
 
 
-
-
-
 const personalMovieDB = {
     counts: numberOfFilms,
     movies: {},
@@ -15,13 +12,57 @@ const personalMovieDB = {
 };
 
 
-const a = prompt("Один из последних просмотренных фильмов", ""),
-      b = prompt("На сколько оцените его", ""),
-      c = prompt("Один из последних просмотренных фильмов", ""),
-      d = prompt("На сколько оцените его", "");
+for (let i = 0; i < 2; i++) {
+    const a = prompt("Один из последних просмотренных фильмов", ""),
+          b = prompt("На сколько оцените его", "");
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }  
+}
+
+// 2 варианта написания цикла через while и do
+// let i = 0;
+// while (i < 2) {
+//     const a = prompt("Один из последних просмотренных фильмов", ""),
+//           b = prompt("На сколько оцените его", "");
+//     i++;
+//     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//         personalMovieDB.movies[a] = b;
+//         console.log('done');
+//     } else {
+//         console.log('error');
+//         i--;
+//     }  
+// }
 
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// do {
+//     const a = prompt("Один из последних просмотренных фильмов", ""),
+//           b = prompt("На сколько оцените его", "");
+//     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//         personalMovieDB.movies[a] = b;
+//         console.log('done');
+//     } else {
+//         console.log('error');
+//         i--;
+//     }  
+//     i++;
+// }
+// while (i < 2);
+
+if (personalMovieDB.counts < 10) {
+    console.log('Просмотренно довольно мало фильмов');
+} else if (personalMovieDB.counts >= 10 && personalMovieDB.counts <= 30) {
+    console.log('Вы классический зритель');
+} else if (personalMovieDB.counts > 30) {
+    console.log('Вы киноман');
+} else {
+    console.log('Произошла ошибка');
+}
 
 console.log(personalMovieDB);
+
